df3b7a618f136786fe503f560f33de72a22f089a
Caio Fischer <caiofis.cf@gmail.com>
2016-03-28 09:57:25 -0300

    Add read line function

--- modules.py
+++ modules.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 def filteredCanny(img):
 	gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) #convert to gray scale
@@ -13,3 +14,15 @@ def filteredCanny(img):
 	thresh_min = thresh_max/2
 	canny = cv2.Canny(filtered,thresh_min,thresh_max)
 	return canny
+
+def ROIfilteredCanny(img,x,y,w,h):
+	roi = img[y:h, x:w]
+	roi = filteredCanny(roi)
+	return roi
+
+def findLine(img,y,h):
+	roi = img[y-(h/2):y+(h/2), 0:]
+	line = roi[h/2]
+	cv2.line(img,(0,y),(640,y),(255,0,0),2)
+	
+	return roi

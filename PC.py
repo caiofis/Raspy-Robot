7aa30c5940cb56addcaa8db8ae576dae12f18b46
Caio Fischer <caiofis.cf@gmail.com>
2016-04-15 11:23:58 -0300

    Find line using horizontal sobel gradient

--- PC.py
+++ PC.py
@@ -8,9 +8,9 @@ while(True):
 	_,frame = cam.read()		#read the image
 	e1 = cv2.getTickCount()
 	#frame = modules.ROIfilteredCanny(frame,0,400,640,450)
-	modules.sobel(frame,400)
+	print modules.readLine(frame,400)
 	e2 = cv2.getTickCount()
-	print (e2-e1)/cv2.getTickFrequency()*1000
+	#print (e2-e1)/cv2.getTickFrequency()*1000
 	cv2.imshow("frame",frame)
 	k = cv2.waitKey(1) & 0xFF
 	if k == 27:	#Tecla esc

b9f13d553f7477f8370191731699c5ec3e8f1148
Caio Fischer <caiofis.cf@gmail.com>
2016-04-18 12:11:54 -0300

    Add checksum to the data package

--- PC.py
+++ PC.py
@@ -1,19 +1,26 @@
 import cv2
 import numpy as np
 import modules
+import comm
+import time
 
-cam = cv2.VideoCapture(0)
+#cam = cv2.VideoCapture(0)
 
 while(True):
-	_,frame = cam.read()		#read the image
+	#_,frame = cam.read()		#read the image
 	e1 = cv2.getTickCount()
-	#frame = modules.ROIfilteredCanny(frame,0,400,640,450)
-	print modules.readLine(frame,400)
+	frdm = comm.FRDM('/dev/ttyACM0')
+	for i in xrange(-10,10,1):
+		frdm.write(M1=0,M2=0,steer=i,leds=4)
+		time.sleep(0.02)
+	for i in xrange(10,-10,-1):
+		frdm.write(M1=0,M2=0,steer=i,leds=4)
+		time.sleep(0.02)
 	e2 = cv2.getTickCount()
 	#print (e2-e1)/cv2.getTickFrequency()
-	cv2.imshow("frame",frame)
-	k = cv2.waitKey(1) & 0xFF
-	if k == 27:	#Tecla esc
-		break
-cam.release()
-cv2.destroyAllWindows()
+	#cv2.imshow("frame",frame)
+	# k = cv2.waitKey(1) & 0xFF
+	# if k == 27:	#Tecla esc
+	# 	break
+# cam.release()
+# cv2.destroyAllWindows()
--- comm.py
+++ comm.py
@@ -1,5 +1,5 @@
 import serial
-
+import time
 
 class FRDM(object):
     """docstring for FRDM"""
@@ -8,17 +8,34 @@ class FRDM(object):
         self.sb = "S"
         self.eb = "E"
         self.commands = [self.sb,'0','0','0','4']
-        self.ser.flush()
+        #self.ser.flush()
 
     def write(self,M1,M2,steer,leds):
-        self.commands[1] =  str(chr(M1+10))
-        self.commands[2] =  str(chr(M2+10))
-        self.commands[3] =  str(chr(steer+10))
+        self.commands[1] =  str((M1+10)).zfill(2)
+        self.commands[2] =  str((M2+10)).zfill(2)
+        self.commands[3] =  str((steer+10)).zfill(2)
         self.commands[4] =  str(leds)
-        for i in self.commands:
-            self.ser.write(i)
-
-
+        self.ser.write(self.commands[0])
+        soma = 0
+        for i in range(1,5,1):
+            self.ser.write(self.commands[i])
+            soma += int(self.commands[i])
+        self.ser.write(str(soma).zfill(2))
 
 frdm = FRDM('/dev/ttyACM0')
-frdm.write(M1=5,M2=0,steer=10,leds=4)
+#frdm.write(M1=0,M2=0,steer=0,leds=4)
+while True:
+    for i in range(-5,5,1):
+        frdm.write(M1=0,M2=0,steer=i,leds=0)
+        frdm.write(M1=0,M2=0,steer=i,leds=0)
+        frdm.write(M1=0,M2=0,steer=i,leds=0)
+        frdm.write(M1=0,M2=0,steer=i,leds=0)
+        time.sleep(0.5)
+        print i
+    for i in range(5,-5,-1):
+        frdm.write(M1=0,M2=0,steer=i,leds=4)
+        frdm.write(M1=0,M2=0,steer=i,leds=4)
+        frdm.write(M1=0,M2=0,steer=i,leds=4)
+        frdm.write(M1=0,M2=0,steer=i,leds=4)
+        time.sleep(0.5)
+        print i

612ea02fc7cc86625efaafa97d3e478ec71f761d
Caio Fischer <caiofis.cf@gmail.com>
2016-04-18 16:13:23 -0300

    Visual testbench to the serial communication protocol

---
+++ testbench.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+import comm
+import time
+
+def nothing(x):
+    pass
+
+# Create a black image, a window
+img = np.zeros((300,512,3), np.uint8)
+cv2.namedWindow('image')
+
+# create trackbars for color change
+cv2.createTrackbar('Motor 1','image',10,20,nothing)
+cv2.createTrackbar('Motor 2','image',10,20,nothing)
+cv2.createTrackbar('Steer   ','image',10,20,nothing)
+#inicialize the serial controler
+controler = comm.FRDM('/dev/ttyACM0')
+while(True):
+    cv2.imshow('image',img)
+    k = cv2.waitKey(1) & 0xFF
+    if k == 27:
+        break
+
+    # get current positions of four trackbars
+    m1 = cv2.getTrackbarPos('Motor 1','image')-10
+    m2 = cv2.getTrackbarPos('Motor 2','image')-10
+    servo = cv2.getTrackbarPos('Steer   ','image')-10
+    print (m1,m2,servo)
+    controler.write(m1,m2,servo,0)
+    time.sleep(0.02)
+cv2.destroyAllWindows()

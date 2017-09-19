9ecf8f5b34663967a29cd34aa4892a8bea4b04b9
Caio <caiofis.cf@gmail.com>
2017-09-19 14:28:45 -0300

    Shuffle the dataset

--- vanilla_net.py
+++ vanilla_net.py
@@ -4,12 +4,19 @@ import matplotlib.pyplot as plt
 
 data = np.load('compressed_data.npy',encoding='bytes') # Load the data
 
-data = np.random.shuffle(data.flat) # Random Shuffle array
+p = np.random.permutation(len(data[0])) # Create a list of random permutations
+data[0] = data[0][p] # Random Shuffle array
+data[1] = data[1][p]
 
 test, training = data[:,:100], data[:,100:]  # Split test and train data
-print test.shape
-print test[0][0].shape
-print test[1][30]
+
+print test[0][0].flatten().shape
+
 plt.imshow(test[0][30])
 plt.show()
+
+x# = tf.placeholder(tf.float32, [None, 784])
+#W = tf.Variable(tf.zeros([784, 10]))
+#b = tf.Variable(tf.zeros([10]))
+#y = tf.nn.softmax(tf.matmul(x, W) + b)
 

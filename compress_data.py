38e3320de0811442fe666a6d57e496fa66129748
Caio <caiofis.cf@gmail.com>
2017-09-25 17:16:03 -0300

    Uses resize instead of pyr

--- compress_data.py
+++ compress_data.py
@@ -8,8 +8,7 @@ data = np.load('data.npy',encoding='bytes') # Load the data
 
 images = []
 for im in data[0]:
-	compressed = cv2.pyrDown(im)
-	compressed = cv2.pyrDown(compressed)
+	compressed = cv2.resize(im,(50,50))
 	images.append(compressed)
 data[0] = images
 np.save('compressed_data',data)
